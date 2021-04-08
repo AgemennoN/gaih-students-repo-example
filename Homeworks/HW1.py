15eb85c55d31b29cd3ed068a79b016987c08b3e0
AgemennoN <81033171+AgemennoN@users.noreply.github.com>
2021-04-08 19:40:24 +0300

    Add files via upload

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,29 @@
 #Explain your work
-
+"""
+										Question 1
+İlk soru için swap array isimli bir fonksiyon tanımladım. Bu fonksiyon arrayin başından uzunluğunun
+yarısına kadar olan değerleri bir geçici değişkene atıyor. Bu atanan kısmı Arrayin geri kalanı ile
+dolduruyor. En son da yarısından sonrası için geçici değişkenden değerleri geri alıyor.
+										Question 2
+Kullanıcıdan değer istedim, bu değere kadar olan tüm sayıların 2 ye bölümünden kalanı 0 olanları bastırdım.
+"""
 #Question 1
-for x in range(a):
-	print(a)
+def swap_array(Array): #Function that swaps the first half of the array with the second half
+	temp = Array[0:int(len(Array)/2)] #int, rounds the float value to an even integer..9/2 = 4.5, int(4.5) = 4
+	Array[0:int(len(Array)/2)] = Array[int(len(Array)/2):len(Array)]
+	Array[int(len(Array) / 2):len(Array)] = temp
+	return Array
+
+print("\nQuestion 1")
+Array = [2,3,4,5,"One","Two","Three","Four","Five"]
+print(Array)
+swap_array(Array)
+print(Array)
+
+#Question 2
+print("\nQuestion 2")
+n = int(input("Enter a single digit number: "))
+for i in range(n+1):
+	if i % 2 == 0:
+		print(i)
+
--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,36 @@
 #Explain your work
+"""
+Kullanıcıdan Username ve Password istedim, Ardından bunları tekrardan girmesini isteyip doğruluğunu
+karşılaştırdım. Extra kısmında bunları Login_infos isimli dictionary oluşturup ilk istediğim username
+ve passwordu içine attım. ikinci sorduklarımı bunlarla karşılaştırdım.
+"""
+#Question
+"""
+Username = input("Enter an Username: ")
+Password = input("Enter an Password: ")
 
-#Question 1
-for x in range(a):
-	print(a)
+Username_toCheck = input("\nEnter Your Username: ")
+
+if Username == Username_toCheck:
+    Password_toCheck = input("Enter Your Password: ")
+    if Password == Password_toCheck:
+        print("Logined Succesfully")
+    else:
+        print("Your Password is incorrect")
+else:
+    print(f'\nYour Username "{Username_toCheck}" does not exist')
+"""
+#Extra Part
+Login_infos = {"Username": "","Password": ""}  #Initilazing the dictionary
+Login_infos["Username"] = input("Enter an Username: ")
+Login_infos["Password"] = input("Enter an Password: ")
+
+Username_toCheck = input("\nEnter Your Username: ")
+
+if Login_infos["Username"] == Username_toCheck:
+    if Login_infos["Password"] == input("Enter Your Password: "):
+        print("Logined Succesfully")
+    else:
+        print("Your Password is incorrect")
+else:
+    print(f'\nYour Username "{Username_toCheck}" does not exist')
